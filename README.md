bd5029fa7594f71578d036338e20af62622eb536
jbfeldman <yohan.feldman@gmail.com>
2017-09-28 20:10:13 -0400

    almost completely finished, adding final touches

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
+Jonah Feldman
+9-28-17
 Sample Code for application to Harvard Technology Instructor internship
 
+Purpose: The Purpose of this program is to create a website where students
+		  can ask questions about their schoolwork, and Teaching Fellows
+		  can look at those questions and take students names off the list
+		  after they have completed a question
 
+Key Files:
+		  home.html: the root page you receive after you access [url]. This file
+		  			displays a list of the waiting students and the questions they asked.
+		  			It also has a name and question box so students can ask their question
+		  			and be put at the end of the queue
+
+		  admin.html: This is the page you receive after accessing [url]/admin. 
+		  			This page is for the Teaching Fellows, and also displays the 
+		  			list of students like the home.html. But unlike home, it 
+		  			contains a remove button for each student so the TFs can 
+		  			remove them from the queue. IT IS ONLY ACCESIBLE WITH A PASSWORD MAYBE REMOVE.
+
+
+		  index.js: this page sets up and runs the server for this program, and
+		  			handles the POST/GET requests from the above html files. 
+		  			The Server is responsible for storing the queue of students/questions,
+		  			and does so in a MongoDB database. In that database, it records the student,
+		  			their question, and the time their question was submitted.
+		  			The Server also uses socket.io to tell the clients when
+		  			the queue is changed, and the pages will reload their queues
+		  			accordingly
+
+		  			
 Notes: Gotta use socket to be able to communicate that you want to update the 
 	list when the instructors change him
 
-TODO:
-
-
-
-
-So we ahve our solution for preventing the page for migrating away
-
-
-home.html is somehow working fine, but in admin.html when you click remove the first time it doens't sucessfully send data
-and if you do it a second time doesn't get caught at all
-
-in the meantime, kill it and revert to previous method in order to fix the rest of the assignment
-
-
-TODO:
-make it delete when you hit remove (fairly simply)
-add socket so the pages update when a deletion happens
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+/*
+ * Jonah Feldman, 9-28-17
+ * index.js
+ * Server file for a Harvard Teaching Fellow Queue
+ */
+
 var express = require('express')
   , http = require('http');
 
 var app = express();
 var server = http.createServer(app);
+
+//sets up socket.io
 var io = require('socket.io').listen(server);
 
 var path = require('path');
@@ -13,7 +21,7 @@ var validator = require('validator');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//set up mongodb database
 var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nodemongoexample';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
@@ -36,26 +44,27 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
+//where you name and question go after you hit submit. Takes a name and question,
+// under the argument request, and uploads them to a mongoDB database
 app.post('/question', function(request, response){
-		var time = new Date();
-		console.log(request.body.name);
-		console.log(request.body.question);
+	var time = new Date();
 
-		if (request.body.name == "ExodiaTheForbiddenOne"){
-			db.collection("students").drop();
-			response.send(200);
-			return;
-		}
+	//get rid of any opportunistic Cross-Site Scripting
+	var name = request.body.name.replace(/[&\/\\#()'":<>{}]/g, '');
+	var question = request.body.question.replace(/[&\/\\#()'":<>{}]/g, '');
 
-		var toInsert = {
-		"name": request.body.name,
-		"quest": request.body.question,
+	var toInsert = {
+		"name": name,
+		"quest": question,
 		"created_at": time.getTime
-		};
+	};
 
 	db.collection("students", function(error, coll){
 		coll.insert(toInsert, function(error, records){
-			response.send(200);
+			response.sendStatus(200);
+
+			//tells the clients (both at home and on teh admin page) that there's
+			//been a change to the question list and it needs to update
 			io.sockets.emit("change");
 		
 		});
@@ -64,31 +73,37 @@ app.post('/question', function(request, response){
 
 });
 
+//removes a student/question from the databse/queue
+// takes as an argument "ID" under request, which contains the MongoDB Document
+// ID of the item we want to remove
 app.post("/remove", function(request, response){
-	console.log(request.body);
-	console.log(request.body.ID);
 	var ObjectId = require('mongodb').ObjectID
 
 	db.collection("students", function(error, coll){
 		coll.remove({_id: ObjectId(request.body.ID)}, function(err, obj){
-			//console.log(doc);
 			response.sendFile(path.join(__dirname + "/public/" + "admin.html"));
+
+			//tells the clients the list of questions has changed, asks them to update
 			io.sockets.emit("change");
 		});
 	});
 	
 	
 });
+
+// returns the admin page when a GET request is recieved for it
 app.get("/admin", function(request, response){
 	response.sendFile(path.join(__dirname + "/public/"+ "/admin.html"));
 });
 
-
+//returns the root page when a GET request is recieved for it
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + "/public/" +"home.html"));
 
 });
 
+//returns an array of objects which contains each student, their corresponding question
+//and the time their question was submitted
 app.post("/queue", function(request, response){
 	response.set('Content-Type', 'text/html');
 
@@ -98,133 +113,5 @@ app.post("/queue", function(request, response){
 			response.send(JSON.stringify(list));
 		});
 	})
-//	var list = [{"name": "Fred", "quest": "Why is the sky blue"}, {"name": "John", "quest": "Why don't girls like me?"}]
-//	response.send(JSON.stringify(list));
 
 });
-/*
-app.get('/vehicle.json', function(request, response){
-	response.setHeader('Content-Type', 'application/json');
-	var driver = request.query.username;
-	if (vehicles.indexOf(driver) != -1){
-		db.collection("vehicles", function(error, coll){
-			if (error){
-	 			console.log("Error: " + error);
-				response.send(500);
-	 		}
-			coll.findOne({"username": driver},function(error, item){
-				if (error){
-	 				console.log("Error: " + error);
-					response.send(500);
-	 			} 
-	 			response.send(item);
-	 		});
-	 	});
-	}
-	else{
-		response.send({});
-	}
-
-
-});
-
-app.post('/submit', function(request, response){
-	//is submitted name a driver or passenger
-	var driver = false;
-	var lat = request.body.lat;
-	var lng = request.body.lng;
-	var username = request.body.username;
-	var time = new Date(); 
-	
-
-	//adjust to EST
-	time.setHours(time.getHours() - time.getTimezoneOffset()/60);
-	var toInsert = {
-		"username": username,
-		"lat": lat,
-		"lng": lng,
-		"created_at": time.toJSON()
-	};
-
-	
-	if (vehicles.indexOf(username) != -1){
-		db.collection('vehicles', function(error, coll) {
-
-			coll.update({"username": username}, toInsert, function(error, updates){
-
-				updates = JSON.parse(updates);
-
-
-				if (error){
-		 			console.log("Error: " + error);
-					response.send(500);
-		 		}
-		    });
-		});
-
-	
-		db.collection("passengers", function(error, coll){
-			if (error){
-	 			console.log("Error: " + error);
-				response.send(500);
-	 		}
-			coll.find().toArray(function(error, items){
-				if (error){
-	 				console.log("Error: " + error);
-					response.send(500);
-	 			}
-				var now = new Date();
-				//adjust to EST
-				now.setHours(now.getHours() - now.getTimezoneOffset()/60);
-				
-				//parse those not matde in last 5 mins
-				for (i = items.length - 1; i >= 0; i--){
-					then = new Date(items[i].created_at);
-					if (now.getTime() - then.getTime() > 300000){
-						items.splice(i,1);
-					}
-				}				
-				items = JSON.stringify(items);
-				response.send('{"passengers": ' + items + "}");
-			});
-		});
-	} 	
-
-
-	//if a passenger
-	else {
-	 	db.collection("passengers", function(error, coll){
-	 		coll.update({"username": username}, toInsert, {upsert: true}, function(error, numUpdated){
-	 			if (error){
-	 				console.log("Error: " + error);
-					response.send(500);
-	 			}
-	 		});
-	 	});
-	 	db.collection("vehicles", function(error, coll){
-			coll.find().toArray(function(error, items){
-				if (error){
-					console.log("error:" + error);
-					response.send(500);
-				}
-				var now = new Date();
-				//adjust to EST
-				now.setHours(now.getHours() - now.getTimezoneOffset()/60);
-				//parse those not matde in last 5 mins
-				for (i = items.length - 1; i >= 0; i--){
-					then = new Date(items[i].created_at);
-					if ( now.getTime() - then.getTime()  > 300000){
-						items.splice(i,1);
-					}
-				}
-				items = JSON.stringify(items);
-				response.send('{"vehicles": ' + items + "}");
-			});
-				
-		});
-	}
-});
-
-	*/
-
-
